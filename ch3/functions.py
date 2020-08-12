a81bf5fea16862f55476f4d7ad2f0cb998c7b5db
Ali <ali@ali.actor>
2020-08-12 23:20:33 +0500

    added length function

--- ch3/functions.py
+++ ch3/functions.py
@@ -1,4 +1,13 @@
+from math import sqrt
+
+
 def add(*vectors):
     coordTuples = zip(*vectors)
     coordSums = [ sum(coords) for coords in coordTuples ]
     return tuple(coordSums)
+
+
+def length(vector):
+    squares = [coord ** 2 for coord in vector]
+    squareSums = sum(squares)
+    return sqrt(squareSums)
