5553f7863f3746a33f8f0697f75d23ebb3ce1b69
Ali <ali@ali.actor>
2020-08-19 00:52:04 +0500

    Written CoordinateVector.py

---
+++ ch06/CoordinateVector.py
@@ -0,0 +1,33 @@
+from abc import abstractmethod
+
+from ch06.Vector import Vector
+from ch06.vectors import add, scale
+
+
+class CoordinateVector(Vector):
+
+    def __init__(self, *coordinates):
+        self.coordinates = tuple(x for x in coordinates)
+
+    @abstractmethod
+    def dimension(self):
+        pass
+
+    def add(self, other):
+        return self.__class__(*add(self.coordinates, other.coordinates))
+
+    def scale(self, s):
+        return self.__class__(*scale(s, self.coordinates))
+
+    def __eq__(self, other):
+        if not self.dimension == other.dimension:
+            return False
+
+        for i in range(0, len(self.coordinates)):
+            if not self.coordinates[i] == other.coordinates[0]:
+                return False
+
+        return True
+
+    def __repr__(self):
+        return "CoordVec({})".format(self.coordinates)
