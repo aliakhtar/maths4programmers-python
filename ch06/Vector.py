71a264028d0b1954b854e6c241db2182cc510dce
Ali <ali@ali.actor>
2020-08-18 22:23:49 +0500

    added vector class with subtract method

---
+++ ch06/Vector.py
@@ -0,0 +1,27 @@
+from abc import ABCMeta, abstractmethod
+
+class Vector(metaclass=ABCMeta):
+    #def __init__(self):
+
+    @abstractmethod
+    def add(self, other):
+        pass
+
+    @abstractmethod
+    def scale(self, other):
+        pass
+
+    def subtract(self, other):
+        return self.add(-1 * other)
+
+    def __sub__(self, other):
+        return self.subtract(other)
+
+    def __add__(self, other):
+        return self.add(other)
+
+    def __mul__(self, s):
+        return self.scale(s)
+
+    def __rmul__(self, s):
+        return self.scale(s)
