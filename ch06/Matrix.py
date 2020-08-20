ab7ad396ccc463f318c62255e2e159f934be8fed
Ali <ali@ali.actor>
2020-08-20 08:44:34 +0500

    Done proj 6.15

--- ch06/Matrix.py
+++ ch06/Matrix.py
@@ -18,10 +18,10 @@ class Matrix(Vector):
 
     def add(self, other):
         new_matrix = tuple(
-            tuple( a + b for (a, b) in zip(row1, row2) )
+            tuple(a + b for (a, b) in zip(row1, row2))
             for row1, row2 in zip(self.matrix, other.matrix)
         )
-        self.__class__(new_matrix)
+        return self.__class__(new_matrix)
 
     def scale(self, s):
         new_matrix = tuple(
@@ -29,7 +29,7 @@ class Matrix(Vector):
             for row in self.matrix
         )
 
-        self.__class__(new_matrix)
+        return self.__class__(new_matrix)
 
     def zero(self):
         new_m = tuple(
@@ -37,3 +37,6 @@ class Matrix(Vector):
             for _ in range(self.rows())
         )
         self.__class__(new_m)
+
+    def __repr__(self):
+        return "{}{}".format(self.__class__.__qualname__, self.matrix)
