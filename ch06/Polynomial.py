e411e525727faa626d30533b71a8a246880b1e4f
Ali <ali@ali.actor>
2020-08-25 11:25:54 +0500

    Reimplemented Polynomial.py to only include coeffs without an extra / specific constant at the end

--- ch06/Polynomial.py
+++ ch06/Polynomial.py
@@ -1,36 +1,31 @@
 from ch06.Vector import Vector
 
 
-# Represents a class which can implement any polynomial function of the type ax**N...bx + c
-# I.e takes in a number of coefficients and a value of x and c,
-# and carries out the equation of adding up all the coefficients and powers of X upto the degree indicated by the
-# number of coefficients. Eg if two coefficients are given, it'll be a quadratic function of the type
-# ax**2 + bx + c
+# Represents a class which can implement any polynomial function of the type ax**1 + ax**2...ax**n
+# I.e takes in a number of coefficients and a value of x and returns the sum of all the powers of x
+#  upto the degree indicated by the number of coefficients.
+#  Eg if two coefficients are given, it'll be a quadratic function of the type
+# ax**2 + bx**1 + c(x**0) = ax**2 + bx + c
 
-# c is a final constant to add to the end of the equation
 class Polynomial(Vector):
 
-    def __init__(self, c, *coefficients):
-        self.c = c
+    def __init__(self, *coefficients):
         self.coeffs = coefficients
 
     def __call__(self, x):
-        to_add = [self.c]
-        for i in range(0, len(self.coeffs)):
-            power_of_x = x ** i
-            to_add.append(power_of_x)
-        return sum(to_add)
+        return sum(
+            (x ** power) * coeff
+            for power, coeff in enumerate(self.coeffs)
+        )
 
     def add(self, other):
-        c = self.c + other.c
         coeffs = [a + b for a, b in zip(self.coeffs, other.coeffs)]
-        return Polynomial(c, coeffs)
+        return Polynomial(coeffs)
 
     def scale(self, s):
-        c = self.c * s
         coeffs = [co * s for co in self.coeffs]
-        return Polynomial(c, coeffs)
+        return Polynomial(coeffs)
 
     @classmethod
     def zero(cls):
-        return Polynomial(0, [0])
+        return Polynomial(0)
