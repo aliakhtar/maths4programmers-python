0ac1e1bcc7cb647c43598afa0f840a6df3d58360
Ali <ali@ali.actor>
2020-08-26 12:43:26 +0500

    Added basic rendering to game.py but not yet working cuz transformed needs to return all the points

--- ch07/asteroids.py
+++ ch07/asteroids.py
@@ -3,6 +3,7 @@ from math import pi, sqrt, cos, sin, atan2
 from random import uniform, randint
 from ch07.vectors import *
 
+
 class PolygonModel():
     def __init__(self, points):
         self.points = points
@@ -10,15 +11,19 @@ class PolygonModel():
         self.x = 0
         self.y = 0
 
+    def transformed(self):
+        return (self.x, self.y)
+
 
 class Ship(PolygonModel):
     def __init__(self):
         super().__init__([(0.5, 0), (-0.25, 0.25), (-0.25, -0.25)])
 
+
 class Asteroid(PolygonModel):
     def __init__(self):
         sides = randint(5, 9)
-        vectors = [to_cartesian( (uniform(0.5, 10), 2 * pi * i / sides) )
+        vectors = [to_cartesian((uniform(0.5, 10), 2 * pi * i / sides))
                    for i in range(sides)]
 
         super().__init__(vectors)
---
+++ ch07/game.py
@@ -0,0 +1,61 @@
+import pygame
+from math import pi, sqrt, cos, sin, atan2
+from random import uniform, randint
+from ch07.vectors import *
+from ch07.asteroids import *
+
+ship = Ship()
+asteroids = [Asteroid() for _ in range(10)]
+
+for a in asteroids:
+    a.x = randint(-9, 9)
+    a.y = randint(-9, 9)
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+width, height = 400, 400
+
+
+def to_pixels(x, y):
+    return (width / 2 + width * x / 20, height / 2 - height * y / 20)
+
+
+def draw_poly(screen, polygon_model, color=GREEN):
+    pixel_points = [to_pixels(x, y) for x, y in polygon_model.transformed()]
+    pygame.draw.aalines(screen, color, True, pixel_points, 10)
+
+
+def draw_segment(screen, v1, v2, color=RED):
+    pygame.draw.aaline(screen, color, to_pixels(*v1), to_pixels(*v2), 10)
+
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode([width, height])
+
+    pygame.display.set_caption("Asteroids!")
+
+    done = False
+    clock = pygame.time.Clock()
+
+    while not done:
+
+        clock.tick()
+        screen.fill(BLACK)
+
+        for a in asteroids:
+            draw_poly(screen, a, color=GREEN)
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+
+if __name__ == "__main__":
+    main()
