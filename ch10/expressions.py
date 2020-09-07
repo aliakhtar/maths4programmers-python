b02b65ef92aa0cf3c1bc5140ff22c6b8b610c7c0
Ali <ali@ali.actor>
2020-09-07 16:43:35 +0500

    Created first pass classes representing expressions

---
+++ ch10/expressions.py
@@ -0,0 +1,35 @@
+class Number():
+    def __init__(self, n):
+        self.number = n
+
+
+class Variable():
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+
+class Power():
+    def __init__(self, base, exponent):
+        self.base = base
+        self.exponent = exponent
+
+
+class Product():
+    def __init__(self, exp1, exp2):
+        self.e1 = exp1
+        self.e2 = exp2
+
+class Sum():
+    def __init__(self, *exps):
+        self.exps = exps
+
+class Function():
+    def __init__(self, name):
+        self.name = name
+
+class Apply():
+    def __init__(self, function, argument):
+        self.f = function
+        self.arg = argument
+
+
