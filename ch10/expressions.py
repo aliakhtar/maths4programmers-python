3c49535d2fa00aee802903a7e47099c866323d54
Ali <ali@ali.actor>
2020-09-08 10:52:49 +0500

    Did first pass expression base class with methods for arithmetic and package function to convert stuff into Expression instances

--- ch10/expressions.py
+++ ch10/expressions.py
@@ -1,53 +1,79 @@
-class Number():
+from abc import ABC
+
+
+def package(item):
+    if isinstance(item, int) or isinstance(item, float):
+        return Number(item)
+    elif isinstance(item, Expression):
+        return item
+    else:
+        raise ValueError("{} could not be packaged, what you doin hoe?".format(item))
+
+
+class Expression(ABC):
+    def __add__(self, other):
+        return Sum(self, package(other))
+
+    def __sub__(self, other):
+        return Difference(self, package(other))
+
+    def __mul__(self, other):
+        return Product(self, package(other))
+
+    def __rmul__(self, other):
+        return Product(package(other), self)
+
+
+class Number(Expression):
     def __init__(self, number):
         self.number = number
 
 
-class Variable():
+class Variable(Expression):
     def __init__(self, symbol):
         self.symbol = symbol
 
 
-class Power():
+class Power(Expression):
     def __init__(self, base, exponent):
         self.exponent = exponent
         self.base = base
 
 
-class Product():
+class Product(Expression):
     def __init__(self, left_e, right_e):
         self.left = left_e
         self.right = right_e
 
 
-class Quotient():
+class Quotient(Expression):
     def __init__(self, numerator, denominator):
         self.numerator = numerator
         self.denominator = denominator
 
 
-class Sum():
+class Sum(Expression):
     def __init__(self, *items):
         self.items = items
 
 
-class Difference():
+class Difference(Expression):
     def __init__(self, bigger, smaller):
         self.bigger = bigger
         self.smaller = smaller
 
 
-class Negative():
+class Negative(Expression):
     def __init__(self, expression):
         self.expression = expression
 
 
-class Function():
+class Function(Expression):
     def __init__(self, name):
         self.items = name
 
 
-class Apply():
+class Apply(Expression):
     def __init__(self, function, arg):
         self.function = function
         self.arg = arg
