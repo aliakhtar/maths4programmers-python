31d928e946536b7231959923d501be887eea22bd
Ali <ali@ali.actor>
2020-09-08 10:43:50 +0500

    Added more missing classes, passing most 10.1 exercises except the last mini project

--- ch10/expressions.py
+++ ch10/expressions.py
@@ -2,30 +2,52 @@ class Number():
     def __init__(self, number):
         self.number = number
 
+
 class Variable():
     def __init__(self, symbol):
         self.symbol = symbol
+
 
 class Power():
     def __init__(self, base, exponent):
         self.exponent = exponent
         self.base = base
 
+
 class Product():
     def __init__(self, left_e, right_e):
         self.left = left_e
         self.right = right_e
 
+
+class Quotient():
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+
+
 class Sum():
     def __init__(self, *items):
         self.items = items
+
+
+class Difference():
+    def __init__(self, bigger, smaller):
+        self.bigger = bigger
+        self.smaller = smaller
+
+
+class Negative():
+    def __init__(self, expression):
+        self.expression = expression
+
 
 class Function():
     def __init__(self, name):
         self.items = name
 
+
 class Apply():
     def __init__(self, function, arg):
         self.function = function
         self.arg = arg
-
