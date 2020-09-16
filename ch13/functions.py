39d45b882fc0c9f42b73cb9352f2bc4a0737284d
Ali <ali@ali.actor>
2020-09-16 16:05:02 +0500

    Wrote make_sinusoid

--- ch13/functions.py
+++ ch13/functions.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from math import sin, cos, pi
 
 
 def plot_function(f, xmin, xmax, **kwargs):
@@ -12,3 +13,8 @@ def plot_sequence(points, max=100, line=False, **kwargs):
         plt.plot(range(0, max), points[0:max], **kwargs)
     else:
         plt.scatter(range(0, max), points[0:max], **kwargs)
+
+
+def make_sinusoid(freq, amp):
+    f = lambda t: amp * sin(freq * 2 * pi * t)
+    return f
