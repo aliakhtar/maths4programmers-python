ee191952271c53aba19194ba880339d1eaad21cd
Ali <ali@ali.actor>
2020-09-16 17:07:44 +0500

    Wrote first pass fourier_method function

--- ch13/functions.py
+++ ch13/functions.py
@@ -25,3 +25,17 @@ def sample(f, start, end, count):
     ts = np.arange(start, end, (end - start) / count)
     values = mapf(ts)
     return values.astype(np.int16)
+
+
+def const(n): return 1
+
+
+def fourier_series(a0, a, b):
+    terms = lambda t, f, items: [itemN * f(2 * pi * t * (index + 1))
+                                 for (index, itemN) in enumerate(items)]
+
+    def result(t):
+        cos_terms = terms(t, cos, a)
+        sin_terms = terms(t, sin, b)
+        return sum(cos_terms, sin_terms) + (a0*const(t))
+    return result
